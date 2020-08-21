2b88c5db75981c9e837a4a207ad00c5779701df7
hjcwinner <hjcwinner@naver.com>
2020-08-21 19:47:39 +0900

    post unlikes와 post comment/:postid/:commentid 차이

--- routes/post.js
+++ routes/post.js
@@ -10,9 +10,6 @@ const checkAuth = passport.authenticate('jwt', { session : false })
 const {
     validPost
 } = require('../helper/validation')
-const post = require('../model/post')
-const profile = require('../model/profile')
-
 
 
 // @route   Url http://localhost:8080/post
@@ -123,28 +120,93 @@ router.post('/likes/:postid', checkAuth, (req, res) => {
                 message : err.message
             })
         })
-
-
-
-
-        // .catch(err => {
-        //     res.status(400).json({
-        //         message : err.message
-        //     })
-        // })
 })
-
-
-
-
-
-
-
-
 
 // @route   Url http://localhost:8080/post/likes/:postid
 // @desc    Delete from user post like
 // @access  private
+router.delete('/unlikes/:postid', checkAuth, (req, res) => {
+    postModel
+        .findById(req.params.postid)
+        .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0)
+            {
+                return res.status(200).json({
+                    message : "no authrized"
+                })
+            }
+            else
+            {
+                removeDate = post.likes
+                    .map(item => item.id)
+                    .indexOf(req.user.id)
+                post.likes.splice(removeDate, 1)
+                post
+                    .save()
+                    .then(post => res.status(200).json(post))
+                    .catch(err => res.status(400).json(err))
+            }
+        })
+        .catch(err => res.status(400).json(err))
+})
+
+// @route   Url http://localhost:8080/post/comments/:postid
+// @desc    Post from user post comments
+// @access  private
+router.post('/comments/:postid', checkAuth, (req, res) => {
+    postModel
+        .findById(req.params.postid)
+        .then(post => {
+            const newCom = {
+                user : req.user.id,
+                text : req.body.text,
+                name : req.user.name,
+                avatar : req.user.avatar
+            }
+
+            post.comments.unshift(newCom)
+            post
+                .save()
+                .then(post => res.status(200).json(post))
+                .catch(err => res.status(404).json(err))
+        })
+        .catch(err => {
+            res.status(400).json({
+                message : err.message
+            })
+        })
+})
+
+// @route   Url http://localhost:8080/post/comments/:postid
+// @desc    Post from user post comments
+// @access  private
+router.delete('/comments/:post_id/:commentid', checkAuth, (req, res) => {
+    postModel
+        .findById(req.params.post_id)
+        .then(post => {
+            if(post.comments.filter(comment => comment.id.toString() === req.params.commentid).length === 0)
+            {
+                return res.status(200).json({
+                    message : "you have not comment this post"
+                })
+            }
+            else
+            {
+                const removePost = post.comments
+                    .map(item => item.id.toString())
+                    .indexOf(req.params.commentid)
+                post.comments.splice(removePost, 1)
+                post
+                    .save()
+                    .then(post => res.status(200).json(post))
+                    .catch(err => res.status(404).json(err))
+            }
+        })
+        .catch(err => res.status(400).json(err))
+})
+
+
+
 
 
 module.exports = router
