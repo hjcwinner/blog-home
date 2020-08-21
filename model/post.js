d5c86190a0c66cbcff9a09a4ce9495e825b6ec00
hjcwinner <hjcwinner@naver.com>
2020-08-21 14:45:52 +0900

    post likes post(좋아요 등록)

---
+++ model/post.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const postSchema = mongoose.Schema(
+    {
+        user : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'user',
+            required : true
+        },
+        text : {
+            type : String,
+            required : true
+        },
+        name : {
+            type : String
+        },
+        avatar : {
+            type : String
+        },
+        likes : [
+            {
+                user : {
+                    type : mongoose.Schema.Types.ObjectId,
+                    ref : 'user',
+                    required : true
+                }
+            }
+        ],
+        comments : []
+    },
+    {
+        timestamps : true
+    }
+)
+
+module.exports = mongoose.model('post', postSchema)
